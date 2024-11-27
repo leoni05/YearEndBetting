8c8bff595b274f4c29eccdbd60115a2a2632af10
yongjun18 <yongjun18976@gmail.com>
2024-11-27 10:49:07 +0900

    git 초기 세팅

---
+++ YearEndBetting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YearEndBetting
+//
+//  Created by yongjun18 on 11/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
