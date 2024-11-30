3967452163bfba8e63411ffe5d1b85d06de022b9
yongjun18 <yongjun18976@gmail.com>
2024-11-30 09:33:42 +0900

    main view controller 세팅

--- YearEndBetting/MainViewController.swift
+++ YearEndBetting/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  YearEndBetting
 //
 //  Created by yongjun18 on 11/27/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +16,3 @@ class ViewController: UIViewController {
 
 
 }
-
