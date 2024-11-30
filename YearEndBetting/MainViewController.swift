04cba8d0d330f38b9846f01a0a27b6443fa0eabc
yongjun18 <yongjun18976@gmail.com>
2024-11-30 11:15:58 +0900

    main upper view ui 세팅

--- YearEndBetting/MainViewController.swift
+++ YearEndBetting/MainViewController.swift
@@ -6,13 +6,54 @@
 //
 
 import UIKit
+import PinLayout
 
 class MainViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    private var upperArea = UIView()
+    private var lowerArea = UIView()
+    
+    private var coinDescLabel = UILabel()
+    private var coinAmountLabel = UILabel()
+    private var greetingLabel = UILabel()
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(upperArea)
+        self.view.addSubview(lowerArea)
+        
+        coinDescLabel.text = "현재 보유 코인"
+        coinDescLabel.textColor = .darkGray
+        coinDescLabel.sizeToFit()
+        upperArea.addSubview(coinDescLabel)
+        
+        coinAmountLabel.text = "1,000,000 AMC"
+        coinAmountLabel.font = .systemFont(ofSize: 30, weight: .heavy)
+        coinAmountLabel.sizeToFit()
+        upperArea.addSubview(coinAmountLabel)
+        
+        greetingLabel.text = "즐거운 연말 보내세요"
+        greetingLabel.font = .systemFont(ofSize: 15)
+        greetingLabel.textColor = .systemGray
+        greetingLabel.sizeToFit()
+        upperArea.addSubview(greetingLabel)
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        upperArea.pin.top(self.view.pin.safeArea)
+            .horizontally(self.view.pin.safeArea).height(200)
+        
+        coinDescLabel.pin.top(70).hCenter()
+        coinAmountLabel.pin.bottom(70).hCenter()
+        greetingLabel.pin.bottom(30).hCenter()
+    }
 
 }
--- YearEndBetting/SceneDelegate.swift
+++ YearEndBetting/SceneDelegate.swift
@@ -15,7 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
+        let nav = UINavigationController(rootViewController: MainViewController())
+        nav.isNavigationBarHidden = true
+        window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
 
