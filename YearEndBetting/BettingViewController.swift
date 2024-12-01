1a7b5d3f04be4c343ee6794f0c7a4cc4c19b0f47
yongjun18 <yongjun18976@gmail.com>
2024-12-01 18:31:33 +0900

    BettingViewController 세팅

---
+++ YearEndBetting/BettingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  BettingViewController.swift
+//  YearEndBetting
+//
+//  Created by yongjun18 on 12/1/24.
+//
+
+import Foundation
+import UIKit
+import PinLayout
+
+class BettingViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private var currentCoinLabel = UILabel()
+    private var askingSelectionLabel = UILabel()
+    
+    // MARK: - Life Cycle
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
+    
+}
