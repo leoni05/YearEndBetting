fa189de09749c25049fea45a1de2d1581aaa3236
yongjun18 <yongjun18976@gmail.com>
2024-12-08 15:03:29 +0900

    순위 화면 상단 Label 세팅

--- YearEndBetting/RankingViewController.swift
+++ YearEndBetting/RankingViewController.swift
@@ -13,6 +13,10 @@ class RankingViewController: UIViewController {
     
     // MARK: - Properties
     
+    private var upperArea = UIView()
+    private var rankDescLabel = UILabel()
+    private var currentRankLabel = UILabel()
+    private var groupNameLabel = UILabel()
     
     
     // MARK: - Life Cycle
@@ -20,11 +24,34 @@ class RankingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        
+        self.view.addSubview(upperArea)
+        
+        rankDescLabel.text = "현재 코인 순위"
+        rankDescLabel.textColor = .darkGray
+        rankDescLabel.sizeToFit()
+        upperArea.addSubview(rankDescLabel)
+        
+        currentRankLabel.text = "1위"
+        currentRankLabel.font = .systemFont(ofSize: 30, weight: .heavy)
+        currentRankLabel.sizeToFit()
+        upperArea.addSubview(currentRankLabel)
+        
+        groupNameLabel.text = "애교가 넘치는 사랑의 하츄핑"
+        groupNameLabel.font = .systemFont(ofSize: 15, weight: .semibold)
+        groupNameLabel.textColor = .systemGray
+        groupNameLabel.sizeToFit()
+        upperArea.addSubview(groupNameLabel)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
+        upperArea.pin.top(self.view.pin.safeArea)
+            .horizontally(self.view.pin.safeArea).height(200)
+        rankDescLabel.pin.top(70).hCenter()
+        currentRankLabel.pin.bottom(70).hCenter()
+        groupNameLabel.pin.bottom(30).hCenter()
     }
     
 }
