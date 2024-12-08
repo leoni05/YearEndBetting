7f8ce877cd8d3054f19f56ace18990e136c91b7e
yongjun18 <yongjun18976@gmail.com>
2024-12-08 23:47:13 +0900

    result group cell 레이아웃 설정

--- YearEndBetting/ResultGroupCell.swift
+++ YearEndBetting/ResultGroupCell.swift
@@ -14,12 +14,16 @@ class ResultGroupCell: UITableViewCell {
     // MARK: - Properties
     
     static let reuseIdentifier = "ResultGroupCell"
-    static let cellHeight = 80.0
+    static let cellHeight = 140.0
     static let cellMarginHorizontal = 16.0
     static let cellMarginVertical = 6.0
     
     private var groupLabel = UILabel()
+    private var gameRankLabel = UILabel()
     private var onePickLabel = UILabel()
+    private var winRewardLabel = UILabel()
+    private var betAmountLabel = UILabel()
+    private var betRewardLabel = UILabel()
     
     // MARK: - Life Cycle
     
@@ -31,14 +35,36 @@ class ResultGroupCell: UITableViewCell {
         contentView.layer.cornerRadius = 10.0
         contentView.clipsToBounds = true
         
-        groupLabel.text = "애교가 넘치는 사랑의 하츄핑"
+        gameRankLabel.text = "게임 1위"
+        gameRankLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        gameRankLabel.textColor = .systemGray2
+        gameRankLabel.sizeToFit()
+        contentView.addSubview(gameRankLabel)
+        
+        groupLabel.text = "애교가 넘치는 사랑의 하츄핑 (1조)"
         groupLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        groupLabel.textColor = UIColor(named: "DarkPink")
         contentView.addSubview(groupLabel)
         
-        onePickLabel.text = "1조"
+        onePickLabel.text = "우리의 원픽   애교가 넘치는 사랑의 하츄핑 (1조)"
         onePickLabel.font = .systemFont(ofSize: 14, weight: .medium)
         onePickLabel.textColor = .darkGray
         contentView.addSubview(onePickLabel)
+        
+        winRewardLabel.text = "승리 보상   +500,000 AMC"
+        winRewardLabel.font = .systemFont(ofSize: 14, weight: .medium)
+        winRewardLabel.textColor = .darkGray
+        contentView.addSubview(winRewardLabel)
+        
+        betAmountLabel.text = "베팅 코인   500,000 AMC"
+        betAmountLabel.font = .systemFont(ofSize: 14, weight: .medium)
+        betAmountLabel.textColor = .darkGray
+        contentView.addSubview(betAmountLabel)
+        
+        betRewardLabel.text = "베팅 보상   +500,000 AMC"
+        betRewardLabel.font = .systemFont(ofSize: 14, weight: .medium)
+        betRewardLabel.textColor = .darkGray
+        contentView.addSubview(betRewardLabel)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -51,8 +77,12 @@ class ResultGroupCell: UITableViewCell {
             by: UIEdgeInsets(top: ResultGroupCell.cellMarginVertical, left: ResultGroupCell.cellMarginHorizontal,
                              bottom: ResultGroupCell.cellMarginVertical, right: ResultGroupCell.cellMarginHorizontal))
         
-        groupLabel.pin.horizontally(15).top(12).sizeToFit(.width)
-        onePickLabel.pin.horizontally(15).bottom(12).sizeToFit(.width)
+        gameRankLabel.pin.right(15).top(12)
+        groupLabel.pin.left(15).before(of: gameRankLabel, aligned: .center).sizeToFit(.width)
+        onePickLabel.pin.horizontally(15).bottom(72).sizeToFit(.width)
+        winRewardLabel.pin.horizontally(15).bottom(52).sizeToFit(.width)
+        betAmountLabel.pin.horizontally(15).bottom(32).sizeToFit(.width)
+        betRewardLabel.pin.horizontally(15).bottom(12).sizeToFit(.width)
     }
 }
 
